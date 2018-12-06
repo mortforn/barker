6326ef1b369a2b52bb445d1d806180c676eb4376
Matthew Fine <matthewfine@Matthews-MacBook-Pro-2.local>
2018-12-06 09:18:54 -0500

    Add styling to Search Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+
+import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Search from './Search';
 import DogList from './DogList';
 import DogDetail from './DogDetail';
+import pf from 'petfinder-client';
+
+const petFinder = pf({
+  key: '<INSERT API KEY HERE>',
+  secret: '<INSERT API SECRET HERE>'
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fetchedDogs: [],
+      likedDogs: [],
+      currentDog: null,
+      dislikedDogs: [] // Array of ids to avoid potential duplicate dog matches
+    };
+  }
+
+  componentDidMount() {
+    petFinder.pet.find({location: '27701'}).then(resp => { // pet.find({animal: 'dog'}) doesn't work :/
+      this.setState({fetchedDogs: resp.petfinder.pets.pet});
+    });
+  }
 
 
-const App = () => (
-  <Switch>
-    <Route exact path='/' component={Search} />
-    <Route path='/matches' component={DogList} />
-    <Route path='/matches/:id' component={DogDetail} />
-  </Switch>
-  // <h1 className="display-1">App Component</h1>
-);
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Search} findDogs={this.componentDidMount()}/>
+        <Route path='/matches' component={DogList} />
+        <Route path='/matches/:id' component={DogDetail} />
+      </Switch>
+    );
+  }
+  
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import '../node_modules/font-awesome/css';
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
