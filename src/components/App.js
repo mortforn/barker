0706b0fe4356e0b349b21b8fe6e81eda6c8fc3ed
Matthew Fine <matthewfine@Matthews-MacBook-Pro-2.local>
2018-12-05 21:35:49 -0500

    Add homepage card display

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,14 @@ import Search from './Search';
 import DogList from './DogList';
 import DogDetail from './DogDetail';
 
+
 const App = () => (
   <Switch>
     <Route exact path='/' component={Search} />
     <Route path='/matches' component={DogList} />
     <Route path='/matches/:id' component={DogDetail} />
   </Switch>
-  <h1 className="display-1">App Component</h1>
+  // <h1 className="display-1">App Component</h1>
 );
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Search = () => (
-  <h1 className="display-1">Search Component</h1>
+  <div className="container container-fluid">
+    <div className='card' id='homepage-card'>
+      <h2 className="display-1">barker.</h2>
+      <input className='form-control'type="text" placeholder='Search for Single Dogs in your Area' />
+      <button className='btn btn-primary' id="search-button">BARK</button>
+    </div>
+  </div>
 );
 
 export default Search;
