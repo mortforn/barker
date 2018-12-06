67f9a74043fea4ee51825f3d1cb0e202fce51b75
Matthew Fine <matthewfine@Matthews-MacBook-Pro-2.local>
2018-12-06 11:08:10 -0500

    comment through notes on potential design pattern moving forward

--- src/components/DogList.js
+++ src/components/DogList.js
@@ -6,13 +6,41 @@ import { Link } from 'react-router-dom'
 
 /*this will be a stateless component that receives the likedDogs array
 as props from the App component*/
-const DogList = () => (
+const DogList = () => {
+
+  //we will need functions to determine the most frequent properties found in the LikedDogs array
+  
+  //const mostFrequentSize
+  //const mostFrequentBreed
+  //const mostFrequentGender
+
+  //organizational function to affect how dogs are displayed
+
+  //function to display detail view
+
+//  const Dawgz = dogList.map((dog) => {
+//     return (
+//       <DogListItem key={dog.id} dog={dog}/>
+//     )
+//   })
+
+
+  return (
+  <div>
   <div className="container container-fluid" id="matches-header">
     <h1 className="display-1">Your Barks!</h1>
     <h3 id="results-lead-up">Based on your results, it looks like your favorite dogs are:</h3>
-    <h4 id='preferences'>size color gender breed</h4>
+    <h4 id='preferences'>size gender breed</h4>
     <Link to='/'><button className='btn btn-primary'>Back</button></Link>
+    </div>
+    <div>
+      <ul>
+        Hello {/* Dawgz */}
+      </ul>
+      {/*This will be the card display for each of the matches*/}
+    </div>
   </div>
-);
+  );
+}
 
 export default DogList;
