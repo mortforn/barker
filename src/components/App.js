530f807841da080fb2a174cf6b22c77ff09e2408
Edward Barton <edward.barton@icloud.com>
2018-12-06 11:31:12 -0500

    Altered App to use hardcoded dog data for now

--- src/components/App.js
+++ src/components/App.js
@@ -3,29 +3,58 @@ import {Switch, Route} from 'react-router-dom';
 import Search from './Search';
 import DogList from './DogList';
 import DogDetail from './DogDetail';
-import pf from 'petfinder-client';
+// import pf from 'petfinder-client';
+import DogMatch from './DogMatch';
+import {dogs} from '../dogs';
 
-const petFinder = pf({
-  key: '<INSERT API KEY HERE>',
-  secret: '<INSERT API SECRET HERE>'
-});
+// const petFinder = pf({
+//   key: '<INSERT API KEY HERE>',
+//   secret: '<INSERT API SECRET HERE>'
+// });
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      fetchedDogs: [],
+      location: '27701',
+      fetchedDogs: dogs,
       likedDogs: [],
-      currentDog: null,
+      currentDog: this.state.fetchedDogs[1],
       dislikedDogs: [] // Array of ids to avoid potential duplicate dog matches
     };
   }
 
-  componentDidMount() {
-    petFinder.pet.find({location: '27701'}).then(resp => { // pet.find({animal: 'dog'}) doesn't work :/
-      this.setState({fetchedDogs: resp.petfinder.pets.pet});
-    });
+  // componentDidMount() {
+  //   petFinder.pet.find({location: this.state.location}).then(resp => { // pet.find({animal: 'dog'}) doesn't work :/
+  //     const dogs = resp.petfinder.pets.pet.map(pet => {
+  //       return {
+  //         age: pet.age,
+  //         name: pet.name,
+  //         breeds: pet.breeds,
+  //         desc: pet.description,
+  //         id: pet.id,
+  //         sex: pet.sex,
+  //         size: pet.size,
+  //         animal: pet.animal,
+  //         images: pet.media.photos
+  //       };
+  //     })
+  //     this.setState({fetchedDogs: dogs});
+  //   });
+  // }
+    
+
+  setLocation = location => {
+    this.setState({location});
+  }
+
+  likeDog = dog => {
+    this.setState({likedDogs: this.state.likedDogs.concat([dog])});
+  }
+
+  dislikeDog = dog => {
+    this.setState({dislikedDogs: this.state.dislikedDogs.concat([dog])});
   }
 
 
@@ -33,6 +62,9 @@ class App extends Component {
     return (
       <Switch>
         <Route exact path='/' component={Search} />
+        <Route path='/match' render={routerProps => (
+          <DogMatch routerProps={routerProps} fetchedDogs={this.state.fetchedDogs} />
+        )} />
         <Route path='/matches' component={DogList} />
         <Route path='/matches/:id' component={DogDetail} />
       </Switch>
