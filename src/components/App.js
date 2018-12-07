6547c83c96c8bdc2e8ebc3cc4de5f7aa24d00a5d
Edward Barton <edward.barton@icloud.com>
2018-12-07 15:28:55 -0500

    Removed redirect

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import Search from './Search';
 import DogList from './DogList';
 import DogDetail from './DogDetail';
